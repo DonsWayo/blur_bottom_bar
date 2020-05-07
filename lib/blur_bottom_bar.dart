083b22814a33ecb4aaa922ed3ed454a1a756b3a6
DonsWayo <djwayomix@gmail.com>
2020-05-07 15:44:41 +0200

    added blur bottom bar widget

--- lib/blur_bottom_bar.dart
+++ lib/blur_bottom_bar.dart
@@ -1,2 +1,59 @@
 library blur_bottom_bar;
 
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class BlurBottomView extends StatelessWidget {
+  final VoidCallback onClick;
+  final double filterX;
+  final double filterY;
+  final double opacity;
+  final Color backgroundColor;
+  final BottomNavigationBarType bottomNavigationBarType;
+  final int currentIndex;
+  final List<BottomNavigationBarItem> bottomNavigationBarItems;
+  final Color selectedItemColor;
+  final Color unselectedItemColor;
+  final Function(int) onIndexChange;
+
+  const BlurBottomView(
+      {Key key,
+      this.onClick,
+      this.filterX = 5.0,
+      this.filterY = 10.0,
+      this.opacity = 0.8,
+      this.backgroundColor = Colors.black,
+      this.bottomNavigationBarType = BottomNavigationBarType.fixed,
+      this.currentIndex = 0,
+      this.onIndexChange,
+      @required this.bottomNavigationBarItems,
+      this.selectedItemColor = Colors.blue,
+      this.unselectedItemColor = Colors.white})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipRect(
+            child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: filterX,
+                  sigmaY: filterY,
+                ),
+                child: Opacity(
+                  opacity: opacity,
+                  child: BottomNavigationBar(
+                    backgroundColor: backgroundColor,
+                    type: bottomNavigationBarType,
+                    items: bottomNavigationBarItems,
+                    currentIndex: currentIndex,
+                    selectedItemColor: selectedItemColor,
+                    unselectedItemColor: unselectedItemColor,
+                    onTap: (index) {
+                      onIndexChange(index);
+                    },
+                  ),
+                ))));
+  }
+}
--- test/blur_bottom_bar_test.dart
+++ test/blur_bottom_bar_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:blur_bottom_bar/blur_bottom_bar.dart';
-
 void main() {
+  /*
   test('adds one to input values', () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
@@ -10,4 +9,5 @@ void main() {
     expect(calculator.addOne(0), 1);
     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
+   */
 }
